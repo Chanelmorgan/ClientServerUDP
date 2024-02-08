8c0e39aaf73f43053511fee0b690cbc6d8bb823d
Chanel Morgan <chanelmorgan05@icloud.com>
2024-02-08 12:30:26 +0000

    Creating the constructor and class variables for the client class

--- src/Client.java
+++ src/Client.java
@@ -1,2 +1,17 @@
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class Client {
+
+    // class variable
+    private DatagramSocket datagramSocket;
+    private InetAddress inetAddress;
+    private byte[] buffer;
+
+    // constructor
+    public Client(DatagramSocket datagramSocket, InetAddress inetAddress){
+        this.datagramSocket = datagramSocket;
+        this.inetAddress = inetAddress;
+    }
+
 }
